8648f1a3a3e0b8d994f7db1f8f84facda264a45d
Adam Podkowinski <adampodkdev@gmail.com>
2021-03-24 15:53:20 +0100

    Init

---
+++ CMakeFiles/sfmlrpg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfmlrpg.dir/src/main.cpp.o"
+  "sfmlrpg"
+  "sfmlrpg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfmlrpg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
